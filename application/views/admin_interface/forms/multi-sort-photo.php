17ee194f8d45bd182cb8a824b93f8dd29d42da58
Vladimir Kharseev <vkharseev@gmail.com>
2013-03-20 12:20:16 +0400

    20.03.2013 12:30

--- application/views/admin_interface/forms/multi-sort-photo.php
+++ application/views/admin_interface/forms/multi-sort-photo.php
@@ -2,7 +2,7 @@
 <?php for($i=0;$i<count($images);$i++):?>
 	<div class="span2 portlet news-image-item" name="image" value="<?=$images[$i]['id'];?>" data-src="<?=$images[$i]['id'];?>">
 		<div class="thumbnail">
-			<div class="portlet-header"><?=$images[$i]['title'];?></div>
+			<div class="portlet-header hidden"><?=$images[$i]['title'];?></div>
 			<div class="portlet-content">
 				<?=anchor('','<img src="'.site_url($images[$i]['src']).'" alt="'.$images[$i]['title'].'" title="'.$images[$i]['title'].'" />',array('class'=>'none'));?>
 			</div>
