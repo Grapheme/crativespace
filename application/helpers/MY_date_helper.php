b8c81f7bd28c7bef554f763d326a8fd09a802763
Vladimir Kharseev <vkharseev@gmail.com>
2013-04-11 15:49:55 +0400

    11.04.2013 16:00

--- application/helpers/MY_date_helper.php
+++ application/helpers/MY_date_helper.php
@@ -55,7 +55,9 @@
 	function month_date($field){
 		
 		$months = array("01"=>"января","02"=>"февраля","03"=>"марта","04"=>"апреля","05"=>"мая","06"=>"июня","07"=>"июля","08"=>"августа","09"=>"сентября","10"=>"октября","11"=>"ноября","12"=>"декабря");
-		$list = preg_split("/-/",$field);
+		$list = explode("-",$field);
+		$list[2] = (int)$list[2];
+		$field = implode("-",$list);
 		$nmonth = $months[$list[1]];
 		$pattern = "/(\d+)(-)(\w+)(-)(\d+)/i";
 		$replacement = "\$5 $nmonth \$1";
